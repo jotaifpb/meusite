9e5fd5b4c5d656f6ca2eebaffe3ee7ed08bb4e80
jotaifpb <87494107+jotaifpb@users.noreply.github.com>
2022-06-03 11:54:41 -0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -8,7 +8,7 @@ $to = "jssilvajoao16@gmail.com";
 $subject = "Mail From codeconia";
 $txt ="Name = ". $name . "\r\n Email = " . $email . "\r\n Message =" . $message;
 \r\n Subject =" . $subject;
-$headers = "From: noreply@codeconia" . "\r\n" .
+$headers = "From: noreply@codeconia.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
